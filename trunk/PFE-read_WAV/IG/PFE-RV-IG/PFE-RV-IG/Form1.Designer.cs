c3dec3dd3ab33b1fe7dcf52c2bb44e3eb70b2cd0
davidlandais <davidlandais@af6524c1-5833-0410-88d9-91f395625675>
2008-01-12 12:40:18 +0000

    * Ma dernière version (interface graphique, automatisation de l'utilisation d'htk ... reste beaucoup de problèmes ... pas touché depuis presque 2 mois).

--- trunk/PFE-read_WAV/IG/PFE-RV-IG/PFE-RV-IG/Form1.Designer.cs
+++ trunk/PFE-read_WAV/IG/PFE-RV-IG/PFE-RV-IG/Form1.Designer.cs
@@ -1,4 +1,4 @@
-namespace WindowsApplication1
+namespace PFE_RV_IG
 {
     partial class Form1
     {
@@ -28,8 +28,13 @@ namespace WindowsApplication1
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.tabControl1 = new System.Windows.Forms.TabControl();
             this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.button1 = new System.Windows.Forms.Button();
+            this.Visualisation = new System.Windows.Forms.GroupBox();
+            this.zedGraphControl1 = new ZedGraph.ZedGraphControl();
+            this.txtout = new System.Windows.Forms.RichTextBox();
             this.tabPage2 = new System.Windows.Forms.TabPage();
             this.tabPage3 = new System.Windows.Forms.TabPage();
             this.tabPage4 = new System.Windows.Forms.TabPage();
@@ -37,8 +42,27 @@ namespace WindowsApplication1
             this.menuStrip1 = new System.Windows.Forms.MenuStrip();
             this.fichierToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.editionToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.statusStrip2 = new System.Windows.Forms.StatusStrip();
+            this.toolStripProgressBar1 = new System.Windows.Forms.ToolStripProgressBar();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.nom = new System.Windows.Forms.Label();
+            this.taille = new System.Windows.Forms.Label();
+            this.fe = new System.Windows.Forms.Label();
+            this.nbbits = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.nbcanaux = new System.Windows.Forms.Label();
             this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.Visualisation.SuspendLayout();
             this.menuStrip1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.statusStrip2.SuspendLayout();
             this.SuspendLayout();
             // 
             // tabControl1
@@ -53,20 +77,69 @@ namespace WindowsApplication1
             this.tabControl1.Location = new System.Drawing.Point(0, 24);
             this.tabControl1.Name = "tabControl1";
             this.tabControl1.SelectedIndex = 0;
-            this.tabControl1.Size = new System.Drawing.Size(672, 359);
+            this.tabControl1.Size = new System.Drawing.Size(737, 426);
             this.tabControl1.TabIndex = 0;
             // 
             // tabPage1
             // 
-            this.tabPage1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.tabPage1.BackColor = System.Drawing.Color.Transparent;
+            this.tabPage1.Controls.Add(this.statusStrip2);
+            this.tabPage1.Controls.Add(this.groupBox1);
+            this.tabPage1.Controls.Add(this.button1);
+            this.tabPage1.Controls.Add(this.Visualisation);
             this.tabPage1.Location = new System.Drawing.Point(4, 25);
             this.tabPage1.Name = "tabPage1";
             this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage1.Size = new System.Drawing.Size(664, 330);
+            this.tabPage1.Size = new System.Drawing.Size(729, 397);
             this.tabPage1.TabIndex = 0;
             this.tabPage1.Text = "Signal Audio";
-            this.tabPage1.UseVisualStyleBackColor = true;
-            this.tabPage1.Click += new System.EventHandler(this.tabPage1_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(11, 10);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(64, 20);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Charger";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Visualisation
+            // 
+            this.Visualisation.BackColor = System.Drawing.Color.Transparent;
+            this.Visualisation.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.Visualisation.Controls.Add(this.zedGraphControl1);
+            this.Visualisation.Controls.Add(this.txtout);
+            this.Visualisation.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.Visualisation.Location = new System.Drawing.Point(352, 6);
+            this.Visualisation.Name = "Visualisation";
+            this.Visualisation.Size = new System.Drawing.Size(369, 351);
+            this.Visualisation.TabIndex = 1;
+            this.Visualisation.TabStop = false;
+            this.Visualisation.Text = "Contenu - Visualisation";
+            // 
+            // zedGraphControl1
+            // 
+            this.zedGraphControl1.Location = new System.Drawing.Point(14, 109);
+            this.zedGraphControl1.Name = "zedGraphControl1";
+            this.zedGraphControl1.ScrollGrace = 0;
+            this.zedGraphControl1.ScrollMaxX = 0;
+            this.zedGraphControl1.ScrollMaxY = 0;
+            this.zedGraphControl1.ScrollMaxY2 = 0;
+            this.zedGraphControl1.ScrollMinX = 0;
+            this.zedGraphControl1.ScrollMinY = 0;
+            this.zedGraphControl1.ScrollMinY2 = 0;
+            this.zedGraphControl1.Size = new System.Drawing.Size(343, 230);
+            this.zedGraphControl1.TabIndex = 0;
+            this.zedGraphControl1.Visible = false;
+            // 
+            // txtout
+            // 
+            this.txtout.Location = new System.Drawing.Point(13, 19);
+            this.txtout.Name = "txtout";
+            this.txtout.Size = new System.Drawing.Size(343, 84);
+            this.txtout.TabIndex = 3;
+            this.txtout.Text = "";
             // 
             // tabPage2
             // 
@@ -74,18 +147,17 @@ namespace WindowsApplication1
             this.tabPage2.Location = new System.Drawing.Point(4, 25);
             this.tabPage2.Name = "tabPage2";
             this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage2.Size = new System.Drawing.Size(664, 330);
+            this.tabPage2.Size = new System.Drawing.Size(729, 397);
             this.tabPage2.TabIndex = 1;
             this.tabPage2.Text = "MFCC";
             this.tabPage2.UseVisualStyleBackColor = true;
-            this.tabPage2.Click += new System.EventHandler(this.tabPage2_Click);
             // 
             // tabPage3
             // 
             this.tabPage3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
             this.tabPage3.Location = new System.Drawing.Point(4, 25);
             this.tabPage3.Name = "tabPage3";
-            this.tabPage3.Size = new System.Drawing.Size(664, 330);
+            this.tabPage3.Size = new System.Drawing.Size(729, 397);
             this.tabPage3.TabIndex = 2;
             this.tabPage3.Text = "Apprentissage";
             // 
@@ -94,7 +166,7 @@ namespace WindowsApplication1
             this.tabPage4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
             this.tabPage4.Location = new System.Drawing.Point(4, 25);
             this.tabPage4.Name = "tabPage4";
-            this.tabPage4.Size = new System.Drawing.Size(664, 330);
+            this.tabPage4.Size = new System.Drawing.Size(729, 397);
             this.tabPage4.TabIndex = 3;
             this.tabPage4.Text = "Tests";
             // 
@@ -103,7 +175,7 @@ namespace WindowsApplication1
             this.tabPage5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
             this.tabPage5.Location = new System.Drawing.Point(4, 25);
             this.tabPage5.Name = "tabPage5";
-            this.tabPage5.Size = new System.Drawing.Size(664, 330);
+            this.tabPage5.Size = new System.Drawing.Size(729, 397);
             this.tabPage5.TabIndex = 4;
             this.tabPage5.Text = "Reconnaissance";
             // 
@@ -114,7 +186,7 @@ namespace WindowsApplication1
             this.editionToolStripMenuItem});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(672, 24);
+            this.menuStrip1.Size = new System.Drawing.Size(737, 24);
             this.menuStrip1.TabIndex = 1;
             this.menuStrip1.Text = "menuStrip1";
             // 
@@ -130,24 +202,172 @@ namespace WindowsApplication1
             this.editionToolStripMenuItem.Size = new System.Drawing.Size(51, 20);
             this.editionToolStripMenuItem.Text = "Edition";
             // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.Color.Transparent;
+            this.groupBox1.Controls.Add(this.nbcanaux);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.nbbits);
+            this.groupBox1.Controls.Add(this.fe);
+            this.groupBox1.Controls.Add(this.taille);
+            this.groupBox1.Controls.Add(this.nom);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(11, 36);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(335, 321);
+            this.groupBox1.TabIndex = 5;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Fichier - Informations";
+            // 
+            // statusStrip2
+            // 
+            this.statusStrip2.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripProgressBar1,
+            this.toolStripStatusLabel1});
+            this.statusStrip2.Location = new System.Drawing.Point(3, 372);
+            this.statusStrip2.Name = "statusStrip2";
+            this.statusStrip2.Size = new System.Drawing.Size(723, 22);
+            this.statusStrip2.TabIndex = 6;
+            this.statusStrip2.Text = "statusStrip2";
+            // 
+            // toolStripProgressBar1
+            // 
+            this.toolStripProgressBar1.Name = "toolStripProgressBar1";
+            this.toolStripProgressBar1.Size = new System.Drawing.Size(100, 16);
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(27, 17);
+            this.toolStripStatusLabel1.Text = "Prêt";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(18, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nom : ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(18, 62);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Taille : ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(18, 93);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(153, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Fréquence d\'échantillonnage : ";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(18, 123);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(199, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Nombre de bits d\'une information audio : ";
+            // 
+            // nom
+            // 
+            this.nom.AutoSize = true;
+            this.nom.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.nom.Location = new System.Drawing.Point(62, 36);
+            this.nom.Name = "nom";
+            this.nom.Size = new System.Drawing.Size(13, 13);
+            this.nom.TabIndex = 4;
+            this.nom.Text = "--";
+            // 
+            // taille
+            // 
+            this.taille.AutoSize = true;
+            this.taille.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.taille.Location = new System.Drawing.Point(65, 62);
+            this.taille.Name = "taille";
+            this.taille.Size = new System.Drawing.Size(13, 13);
+            this.taille.TabIndex = 5;
+            this.taille.Text = "--";
+            // 
+            // fe
+            // 
+            this.fe.AutoSize = true;
+            this.fe.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.fe.Location = new System.Drawing.Point(177, 93);
+            this.fe.Name = "fe";
+            this.fe.Size = new System.Drawing.Size(13, 13);
+            this.fe.TabIndex = 6;
+            this.fe.Text = "--";
+            // 
+            // nbbits
+            // 
+            this.nbbits.AutoSize = true;
+            this.nbbits.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.nbbits.Location = new System.Drawing.Point(223, 123);
+            this.nbbits.Name = "nbbits";
+            this.nbbits.Size = new System.Drawing.Size(13, 13);
+            this.nbbits.TabIndex = 7;
+            this.nbbits.Text = "--";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(18, 156);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(106, 13);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Nombre de canaux : ";
+            // 
+            // nbcanaux
+            // 
+            this.nbcanaux.AutoSize = true;
+            this.nbcanaux.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.nbcanaux.Location = new System.Drawing.Point(130, 156);
+            this.nbcanaux.Name = "nbcanaux";
+            this.nbcanaux.Size = new System.Drawing.Size(13, 13);
+            this.nbcanaux.TabIndex = 9;
+            this.nbcanaux.Text = "--";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
-            this.ClientSize = new System.Drawing.Size(672, 383);
+            this.ClientSize = new System.Drawing.Size(737, 450);
             this.Controls.Add(this.tabControl1);
             this.Controls.Add(this.menuStrip1);
             this.MainMenuStrip = this.menuStrip1;
             this.MaximizeBox = false;
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "PFE - Apprentissage";
+            this.Text = "PFE - Reconnaissance vocale";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.Visualisation.ResumeLayout(false);
             this.menuStrip1.ResumeLayout(false);
             this.menuStrip1.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.statusStrip2.ResumeLayout(false);
+            this.statusStrip2.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -164,6 +384,25 @@ namespace WindowsApplication1
         private System.Windows.Forms.TabPage tabPage3;
         private System.Windows.Forms.TabPage tabPage4;
         private System.Windows.Forms.TabPage tabPage5;
+        private ZedGraph.ZedGraphControl zedGraphControl1;
+        private System.Windows.Forms.GroupBox Visualisation;
+        private System.Windows.Forms.RichTextBox txtout;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.StatusStrip statusStrip2;
+        private System.Windows.Forms.ToolStripProgressBar toolStripProgressBar1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Label nbbits;
+        private System.Windows.Forms.Label fe;
+        private System.Windows.Forms.Label taille;
+        private System.Windows.Forms.Label nom;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label nbcanaux;
+        private System.Windows.Forms.Label label5;
 
     }
 }
--- trunk/PFE-read_WAV/IG/PFE-RV-IG/PFE-RV-IG/Form1.cs
+++ trunk/PFE-read_WAV/IG/PFE-RV-IG/PFE-RV-IG/Form1.cs
@@ -5,8 +5,9 @@ using System.Data;
 using System.Drawing;
 using System.Text;
 using System.Windows.Forms;
+using ZedGraph;
 
-namespace WindowsApplication1
+namespace PFE_RV_IG
 {
     public partial class Form1 : Form
     {
@@ -15,19 +16,138 @@ namespace WindowsApplication1
             InitializeComponent();
         }
 
+        // Respond to the form 'Resize' event
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            //SetSize();
+        }
+
+        // SetSize() is separate from Resize() so we can 
+        // call it independently from the Form1_Load() method
+        // This leaves a 10 px margin around the outside of the control
+        // Customize this to fit your needs
+        private void SetSize()
+        {
+            zedGraphControl1.Location = new Point(10, 10);
+            // Leave a small margin around the outside of the control
+            zedGraphControl1.Size = new Size(ClientRectangle.Width - 20,
+                                    ClientRectangle.Height - 20);
+        }
+
+        // Respond to form 'Load' event
         private void Form1_Load(object sender, EventArgs e)
         {
-
+            // Setup the graph
+            //CreateGraph(zedGraphControl1);
+            // Size the control to fill the form with a margin
+            //SetSize();
         }
 
-        private void tabPage2_Click(object sender, EventArgs e)
+        // Build the Chart
+        private void CreateGraph(ZedGraphControl zgc)
         {
+            // get a reference to the GraphPane
+            GraphPane myPane = zgc.GraphPane;
 
+            // Set the Titles
+            myPane.Title.Text = "My Test Graph\n(For CodeProject Sample)";
+            myPane.XAxis.Title.Text = "My X Axis";
+            myPane.YAxis.Title.Text = "My Y Axis";
+
+            // Make up some data arrays based on the Sine function
+            double x, y1, y2;
+            PointPairList list1 = new PointPairList();
+            PointPairList list2 = new PointPairList();
+            for (int i = 0; i < 36; i++)
+            {
+                x = (double)i + 5;
+                y1 = 1.5 + Math.Sin((double)i * 0.2);
+                y2 = 3.0 * (1.5 + Math.Sin((double)i * 0.2));
+                list1.Add(x, y1);
+                list2.Add(x, y2);
+            }
+
+            // Generate a red curve with diamond
+            // symbols, and "Porsche" in the legend
+            LineItem myCurve = myPane.AddCurve("Porsche",
+                  list1, Color.Red, SymbolType.Diamond);
+
+            // Generate a blue curve with circle
+            // symbols, and "Piper" in the legend
+            LineItem myCurve2 = myPane.AddCurve("Piper",
+                  list2, Color.Blue, SymbolType.Circle);
+            // Tell ZedGraph to refigure the
+            // axes since the data have changed
+            zgc.AxisChange();
         }
 
-        private void tabPage1_Click(object sender, EventArgs e)
+        private void UpdateGraph(ZedGraphControl zgc)
         {
+            // get a reference to the GraphPane
+            GraphPane myPane = zgc.GraphPane;
 
+            // Set the Titles
+            myPane.Title.Text = "Contenu fichier WAV";
+            myPane.XAxis.Title.FontSpec.IsBold = true;
+            myPane.XAxis.Title.FontSpec.Size = 18;
+            myPane.YAxis.Title.FontSpec.Size = 18;
+            myPane.Title.FontSpec.FontColor = Color.Red;
+            myPane.Title.FontSpec.Size = 22;
+
+            myPane.XAxis.Title.Text = "";
+            myPane.YAxis.Title.Text = "";
+            myPane.XAxis.Scale.IsUseTenPower = true;
+            myPane.YAxis.Scale.IsUseTenPower = true;
+   
+            // Make up some data arrays based on the Sine function
+ 
+            double x, y1, y2;
+            PointPairList list1 = new PointPairList();
+            PointPairList list2 = new PointPairList();
+            for (int i = 0; i < wav_.data_length_get(); i++)
+            {
+                list1.Add(i, wav_.data_get()[i]);
+            }
+
+            // Generate a red curve with diamond
+            // symbols, and "Porsche" in the legend
+            LineItem myCurve = myPane.AddCurve("",
+                  list1, Color.FromArgb(100, 10, 200), SymbolType.None);
+            myPane.Fill = (new Fill(Color.White, Color.White, 0));
+            myPane.Chart.Fill = (new Fill(Color.FromArgb(192, 192, 255), Color.FromArgb(255, 255, 210), -45F));
+            
+            // Generate a blue curve with circle
+            // symbols, and "Piper" in the legend
+            //LineItem myCurve2 = myPane.AddCurve("Piper",
+            //      list2, Color.Blue, SymbolType.Circle);
+
+            // Tell ZedGraph to refigure the
+            // axes since the data have changed
+            zgc.AxisChange();
+            zgc.Refresh(); // Invalidate();
+        }
+
+        public Wav wav_;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                wav_ = new Wav(openFileDialog1.FileName);
+                txtout.AppendText(Convert.ToString(wav_.data_length_get()) + "\n-------\nValeurs\n-------\n");
+                for (int i = 0; i < wav_.data_length_get(); ++i)
+                {
+                    txtout.AppendText(Convert.ToString(wav_.data_get()[i]) + "\n");
+                }
+                UpdateGraph(zedGraphControl1);
+                zedGraphControl1.Visible = true;
+                String[] tmp = openFileDialog1.FileName.Split('\\');
+                nom.Text = tmp[tmp.Length - 1];
+                taille.Text = Convert.ToString(wav_.header_get().dLen);
+                fe.Text = Convert.ToString(wav_.header_get().nSamplesPerSec);
+                nbbits.Text = Convert.ToString(wav_.header_get().nBitsPerSample);
+                nbcanaux.Text = Convert.ToString(wav_.header_get().nChannels);
+            }
         }
     }
 }
